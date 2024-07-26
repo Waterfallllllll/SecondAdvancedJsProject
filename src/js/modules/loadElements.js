a48d13cf037d56f3c83a83b6fc9decc11adedaa9
Turcan Ilya <ilieturcan424@gmail.com>
2024-07-26 19:23:02 +0300

    Add loadElements v1.9.0

--- src/js/modules/loadElements.js
+++ src/js/modules/loadElements.js
@@ -11,7 +11,7 @@ const loadElements = (button, elements) => {
 			item.classList.remove("hidden-lg", "hidden-md", "hidden-sm", "hidden-xs", "styles-2");
 			item.classList.add("col-sm-3", "col-sm-offset-0", "col-xs-10", "col-xs-offset-1");
 		});
-
+		
 		btn.style.display = "none";
 	});
 };
